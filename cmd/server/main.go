def73416c9ebdcd06373de73fd1752f30a38d52c
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2025-10-29 10:43:48 -0600

    CH3: Publishers & Queues - Decoupling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -25,19 +26,53 @@ func main() {
 		log.Fatalf("could not open channel: %v", err)
 	}
 
-	err = pubsub.PublishJSON(
-		ch,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	)
-	if err != nil {
-		log.Fatalf("could not publish message: %v", err)
-	}
-	fmt.Println("Pause message sent!")
+	gamelogic.PrintServerHelp()
 
+	for {
+		userInput := gamelogic.GetInput()
+
+		if len(userInput) == 0 {
+			continue
+		}
+
+		switch userInput[0] {
+		case "pause":
+			fmt.Println("Sending pause message...")
+			err = pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Fatalf("could not publish message: %v", err)
+			}
+		
+		case "resume":
+			fmt.Println("Sending resume message...")
+			err = pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Fatalf("could not publish message: %v", err)
+			}
+		
+		case "quit":
+			fmt.Println("Quiting the game...")
+			return
+
+		default:
+			fmt.Println("Unknown command!")
+		}
+	}
+	
 	// wait for ctrl+c
 	// signalChan := make(chan os.Signal, 1)
 	// signal.Notify(signalChan, os.Interrupt)
