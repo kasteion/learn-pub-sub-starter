f5f3ce4a2036041355cf74fbabd90b8da4d83c57
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2025-10-31 16:08:41 -0600

    CH3: Publishers & Queues - Durable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -26,6 +26,18 @@ func main() {
 		log.Fatalf("could not open channel: %v", err)
 	}
 
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug + ".*",
+		pubsub.SimpleQueueDurable,
+	)
+	if err != nil {
+		log.Fatalf("could not subscribe to %s: %v", routing.GameLogSlug + ".*", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
 	gamelogic.PrintServerHelp()
 
 	for {
